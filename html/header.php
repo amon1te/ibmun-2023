4fdae95aa5ee745898eb09940849df15cc5497b9
amon1te <alenshakeyev@gmail.com>
2023-03-14 01:05:40 +0600

    about us page 80%

---
+++ html/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/styles-header.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
+    <link rel="shortcut icon" href="/public/Images/Ecojeli_logo_trans.png" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <title>Ecojeli</title>
+</head>
+<body>
+    <header>
+        <div class="logo">
+        </div>
+        <div class="hamburger">
+            <div class="line"></div>
+            <div class="line"></div>
+            <div class="line"></div>
+        </div>
+        <nav class="nav-bar">
+            <ul>
+                <li>
+                    <a href="/public/html/index.php" class="not-login">Main</a>
+                </li>
+                <li>
+                    <a class="not-login" href="/public/html/aboutUs.php">About Us</a>
+                </li>
+                <li>
+                    <a class="not-login" href="/public/html/contact.php">News</a>
+                </li>
+                <li>
+                    <a href="/public/html/login.php" target="_blank">
+                        <button type="button" class="btn">Log in</button>
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <div class="empty"></div>
---
+++ html/our-team.php
@@ -0,0 +1,140 @@
+<?php 
+    include_once 'header.php';
+?>
+<link rel="stylesheet" href="/css/styles-our-team.css">
+
+        <div class="title">Our staff</div>
+        <div class="roles">
+
+            <h1>Core Secretariat</h1>
+            <div class="core-secretariat">
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8657 (2).jpg" alt="Aidana Valikhan">
+                    </div>
+                    <p class="name">Aidana Valikhan</p>
+                    <p class="sub-role">Director General</p>
+                    <p class="sub-role">Financial Manager</p>
+                </div>
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8740.jpg" alt="Murat Almas">
+                    </div>
+                    <p class="name">Murat Almas</p>
+                    <p class="sub-role">Secretary General</p>
+                </div>
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8682.jpg" alt="Mira Sultangaliyeva">
+                    </div>
+                    <p class="name">Mira Sultangaliyeva</p>
+                    <p class="sub-role">Chief of Staff</p>
+                </div>
+            </div>
+
+
+            <h1>Supervisors</h1>
+            <div class="supervisors">
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/super1.jpg" alt="Zhikibekova Kuralay">
+                    </div>
+                    <p class="name">Zhikibekova Kuralay</p>
+                    <p class="sub-role">Supervisor</p>
+                </div>
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/super2.jpg" alt="Sarsenbekova Symbat">
+                    </div>
+                    <p class="name">Sarsenbekova Symbat</p>
+                    <p class="sub-role">Supervisor</p>
+                </div>
+            </div>
+
+
+            <h1>Educational Leaders</h1>
+            <div class="educational-leaders">
+                <div class="card">
+                    <div class="photo"></div>
+                    <p class="name"></p>
+                    <p class="sub-role"></p>
+                </div>
+            </div>
+
+
+            <h1>Delegate Affairs</h1>
+            <div class="delegate-affairs">
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8734.jpg" alt="Myrzabekova Aidana">
+                    </div>
+                    <p class="name">Myrzabekova Aidana</p>
+                    <p class="sub-role">Educational Leader of Delegates</p>
+                    <p class="sub-role">Chair of Unicef Committee</p>
+                </div>
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8708.jpg" alt="Mukhamedzhanov Arsen">
+                    </div>
+                    <p id="arsen" class="name">Mukhamedzhanov Arsen</p>
+                    <p class="sub-role">Delegate Affairs Commissioner</p>
+                    <p class="sub-role">Financial Manager</p>
+                    <p class="sub-role">Co-Chair of UNDP Committee</p>
+                </div>
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8630.jpg" alt="Maulenova Zarina">
+                    </div>
+                    <p class="name">Maulenova Zarina</p>
+                    <p class="sub-role">Delegate Affairs Commissioner</p>
+                    <p class="sub-role">Financial Manager</p>
+                    <p class="sub-role">Co-Chair of UNDP Committee</p>
+                </div>
+            </div>
+
+
+            <h1>SMM Developers</h1>
+            <div class="smm-dev">
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8661.jpg" alt="Usein Zarema">
+                    </div>
+                    <p class="name">Usein Zarema</p>
+                    <p class="sub-role">SMM reels-maker</p>
+                </div>
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8721.jpg" alt="Muratkyzy Elnaz">
+                    </div>
+                    <p class="name">Muratkyzy Elnaz</p>
+                    <p class="sub-role">SMM stories-maker</p>
+                </div>
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8642 (3).jpg" alt="Bolat Aisha">
+                    </div>
+                    <p class="name">Bolat Aisha</p>
+                    <p class="sub-role">SMM stories-maker</p>
+                </div>
+            </div>
+
+            <h1>IT Specialists</h1>
+            <div class="it-specialists">
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/IMG_8704.jpg" alt="Ausarbekov Adilet">
+                    </div>
+                    <p class="name">Ausarbekov Adilet</p>
+                    <p class="sub-role">Back-end Developer</p>
+                </div>
+                <div class="card">
+                    <div class="photo">
+                        <img src="/images/83bdec6b187e53e1d14a79e8e013bc60.jpg" alt="Unknown">
+                    </div>
+                    <p class="name">Unknown</p>
+                    <p class="sub-role">Front-end Developer</p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
