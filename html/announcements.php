89aae073688b2f9cc7005de38f0422f6eb321980
amon1te <alenshakeyev@gmail.com>
2023-04-01 23:58:57 +0600

    +updates

--- html/announcements.php
+++ html/announcements.php
@@ -1,17 +1,32 @@
 <?php 
     include_once 'header.php';
     ?>
+    
 <link rel="stylesheet" href="/css/styles-ann.css">
 
 <div class="main">
-    <div class="unit">
+    <div class="unit" id="id20032023">
         <h1>Announcement</h1>
         <p class="date">20.03.2023</p>
-        <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur tempore laudantium illum alias molestias expedita tempora officiis totam in cupiditate!</p>
+        <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, corrupti molestias perspiciatis adipisci omnis illo eveniet ab aut placeat ullam. Tempore, nisi provident! Nostrum doloribus modi consectetur officia, sapiente, molestias et quos ullam ab repellendus fuga corrupti, eligendi itaque temporibus. Qui voluptatum, illum obcaecati ut libero sit pariatur hic suscipit eaque, sequi repellendus reiciendis, dicta facere in id ea necessitatibus maxime odio! Tempore, porro quod tenetur, repellat quasi ex adipisci quibusdam voluptates sit expedita sint quo soluta architecto excepturi ipsam placeat fugiat dolore pariatur quae! Nostrum, dolorem iusto enim sint neque voluptas, fuga possimus debitis dolor illo, minus tenetur optio.</p>
+        <input type="checkbox" class="expand-btn">
+    </div>
+    <div class="unit" id="id19032022">
+        <h1>Announcement</h1>
+        <p class="date">19.03.2022</p>
+        <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, corrupti molestias perspiciatis adipisci omnis illo eveniet ab aut placeat ullam. Tempore, nisi provident! Nostrum doloribus modi consectetur officia, sapiente, molestias et quos ullam ab repellendus fuga corrupti, eligendi itaque temporibus. Qui voluptatum, illum obcaecati ut libero sit pariatur hic suscipit eaque, sequi repellendus reiciendis, dicta facere in id ea necessitatibus maxime odio! Tempore, porro quod tenetur, repellat quasi ex adipisci quibusdam voluptates sit expedita sint quo soluta architecto excepturi ipsam placeat fugiat dolore pariatur quae! Nostrum, dolorem iusto enim sint neque voluptas, fuga possimus debitis dolor illo, minus tenetur optio.</p>
+        <input type="checkbox" class="expand-btn">
+    </div>
+    <div class="unit" id="id19032022">
+        <h1>Announcement</h1>
+        <p class="date">19.03.2022</p>
+        <p class="info">Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum, corrupti molestias perspiciatis adipisci omnis illo eveniet ab aut placeat ullam. Tempore, nisi provident! Nostrum doloribus modi consectetur officia, sapiente, molestias et quos ullam ab repellendus fuga corrupti, eligendi itaque temporibus. Qui voluptatum, illum obcaecati ut libero sit pariatur hic suscipit eaque, sequi repellendus reiciendis, dicta facere in id ea necessitatibus maxime odio! Tempore, porro quod tenetur, repellat quasi ex adipisci quibusdam voluptates sit expedita sint quo soluta architecto excepturi ipsam placeat fugiat dolore pariatur quae! Nostrum, dolorem iusto enim sint neque voluptas, fuga possimus debitis dolor illo, minus tenetur optio.</p>
+        <input type="checkbox" class="expand-btn">
     </div>
 </div>
 </body>
 </html>
+
 <?php 
     include_once 'footer.php';
     ?>
--- html/header.php
+++ html/header.php
@@ -9,7 +9,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Rubik&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
-    <link rel="shortcut icon" href="/public/Images/Ecojeli_logo_trans.png" type="image/x-icon">
+    <link rel="shortcut icon" href="/images/ibmunwhite.png" type="image/x-icon">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <title>IBeeMUN</title>
